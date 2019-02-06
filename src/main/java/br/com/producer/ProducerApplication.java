1056113d668b8a06aa7de9451f17b9b36ca626a5
Diego Almeida <dgoalmeida@gmail.com>
2019-02-06 07:56:10 -0200

    producer estudo

---
+++ src/main/java/br/com/producer/ProducerApplication.java
@@ -0,0 +1,17 @@
+package br.com.producer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+@EnableAutoConfiguration
+
+public class ProducerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProducerApplication.class, args);
+	}
+
+}
+
---
+++ src/main/java/br/com/producer/resource/ProducerResource.java
@@ -0,0 +1,26 @@
+package br.com.producer.resource;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("kafka")
+public class ProducerResource {
+
+    @Autowired
+    private KafkaTemplate<String, String> kafkaTemplate;
+
+    private static final String TOPIC = "Kafka_Example";
+
+    @GetMapping("/publish/{name}")
+    public String post(@PathVariable("name") final String name) {
+
+        kafkaTemplate.send(TOPIC, name);
+
+        return "Published successfully";
+    }
+}
